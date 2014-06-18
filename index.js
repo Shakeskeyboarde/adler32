9585e18f1c55a997ece3e2a5aacf32a5b83b4ada
Chris Ackerman <chris@bonzidev.com>
2014-06-18 16:50:52 -0700

    All tests working now.

--- index.js
+++ index.js
@@ -13,23 +13,26 @@ var BASE = 65521;
  * integer. This is an optimization. We "could" take the modulo after each byte, and it must be taken before each
  * digest.
  */
-var NMAX = 5521;
+var NMAX = 5552;
 
-exports.sum = function(buf)
+exports.sum = function(buf, adler)
 {
-	var a = 1,
-		b = 0,
+	if (adler == null)
+		adler = 1;
+
+	var a = adler & 0xFFFF,
+		b = (adler >>> 16) & 0xFFFF,
 		i = 0,
 		max = buf.length,
 		n, value;
 
-	for (; i < max; ++i)
+	while (i < max)
 	{
-		n = Math.min(NMAX, max - 1);
+		n = Math.min(NMAX, max - i);
 
 		do
 		{
-			a += buf[i];
+			a += buf[i++]<<0;
 			b += a;
 		}
 		while (--n);
@@ -41,25 +44,21 @@ exports.sum = function(buf)
 	return ((b << 16) | a) >>> 0;
 };
 
-exports.concat = function(sum0, sum1, length1)
-{
-	var a0 = sum0 & 0xFFFF,
-		b0 = (sum0 >>> 16) & 0xFFFF,
-		a1 = sum1 & 0xFFFF,
-		b1 = (sum1 >>> 16) & 0xFFFF,
-		a = (a0 + a1) % BASE,
-		b = ((b0 * length1) + b1) % BASE;
-
-	return ((b << 16) | a) >>> 0;
-};
-
 exports.roll = function(sum, length, oldByte, newByte)
 {
 	var a = sum & 0xFFFF,
 		b = (sum >>> 16) & 0xFFFF;
 
-	a = (a - oldByte + newByte) % BASE;
-	b = (b - (length * oldByte) + a) % BASE;
+	if (newByte != null)
+	{
+		a = (a - oldByte + newByte + BASE) % BASE;
+		b = (b - ((length * oldByte) % BASE) + a - 1 + BASE) % BASE;
+	}
+	else
+	{
+		a = (a - oldByte + BASE) % BASE;
+		b = (b - ((length * oldByte) % BASE) - 1 + BASE) % BASE;
+	}
 
 	return ((b << 16) | a) >>> 0;
 };
--- test.js
+++ test.js
@@ -1,3 +1,76 @@
 #!/usr/bin/env node
-"use strict";
 
+var should = require('should');
+var fs = require('fs');
+var Adler32 = require('./');
+var hash = require('mhash').hash;
+
+var buf = fs.readFileSync(__filename);
+
+describe('Adler32', function() {
+	describe('.sum(buf, adler = 1)', function() {
+		var sum = Adler32.sum(buf);
+
+		it('should return a number above 0 and below 4293984241', function() {
+			sum.should.be.above(0);
+			sum.should.be.below(4293984241);
+		});
+
+		it('should match the sum calculated by mhash', function() {
+			sum.should.be.exactly(parseInt(hash('adler32', buf), 16));
+		});
+
+		it('should be the same if done as a running sum', function() {
+			var half = (buf.length / 2)<<0;
+			var part0 = buf.slice(0, half);
+			var part1 = buf.slice(half);
+			var partial = Adler32.sum(part0);
+			var sum0 = Adler32.sum(buf);
+			var sum1 = Adler32.sum(part1, partial);
+
+			sum1.should.be.exactly(sum0);
+		});
+	});
+
+	describe('.roll(sum, length, oldByte, newByte = null)', function() {
+		it('should result in the same value as sum for each offset chunk of size 64', function() {
+			rollTest(64);
+		});
+
+		it('should result in the same value as sum for each offset chunk of size 128', function() {
+			rollTest(128);
+		});
+
+		it('should result in the same value as sum for each offset chunk of size 256', function() {
+			rollTest(256);
+		});
+
+		it('should result in the same value as sum for each offset chunk of size 512', function() {
+			rollTest(512);
+		});
+
+		it('should result in the same value as sum for each offset chunk of size 1024', function() {
+			rollTest(1024);
+		});
+
+		it('should result in the same value as sum for each offset chunk of size 2048', function() {
+			rollTest(2048);
+		});
+
+		it('should result in the same value as sum for each offset chunk of size 4096', function() {
+			rollTest(4096);
+		});
+	});
+});
+
+function rollTest(chunkSize)
+{
+	var sum = Adler32.sum(buf.slice(0, chunkSize));
+	var i = 0;
+
+	for (; i < buf.length; ++i)
+	{
+		sum = Adler32.roll(sum, Math.min(chunkSize, buf.length - i), buf[i], buf[i + chunkSize]);
+		sum.should.be.exactly(Adler32.sum(buf.slice(i + 1, i + 1 + chunkSize)));
+	}
+}
